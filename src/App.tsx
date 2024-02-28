cdb4f86f9549f082e23448e82f949ea39fedd2f8
unknown <andressapatricio93@gmail.com>
2024-02-28 13:22:49 -0300

    Adicionando CRUD Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import './App.css';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaCategorias from './components/categories/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categories/formularioCategorias/FormularioCategorias';
+import DeletarCategoria from './components/categories/deletarCategorias/DeletarCategorias';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+   <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
 );
 }
